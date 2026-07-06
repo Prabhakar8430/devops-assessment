a6ec02e75e60d5a2387acdc224aff4276785b98d
Prabhakar <prabhakaragre99@gmail.com>
2026-07-07 02:57:08 +0530

    test workflows

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,8 +1,11 @@
 module "network" {
-
   source = "../../modules/network"
 
-  ...
+  environment        = var.environment
+  vpc_cidr           = var.vpc_cidr
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
+  availability_zones = var.availability_zones
 }
 module "ecs" {
 
